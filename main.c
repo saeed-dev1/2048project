6d4da3e85f995d04b9dddf379cdc6e1db9b8faa2
DESKTOP-EV1FOLV\Sazgar <saeedqorbaly@gmail.com>
2023-07-06 03:46:30 +0330

    login option was created :)

--- main.c
+++ main.c
@@ -4,28 +4,21 @@
 #include<unistd.h>
 
 int i = 1 ;
+int authentication = 0 ;
 FILE * fp ;
+
+
 typedef struct user
 {
     char name[50] ;
-    char password[50] ;
-    //struct user * next ; 
+    char password[50] ; 
 } User ;
-
-/*User create_new_user(char u_name[50] , char pswd[50])
-{
-    User u1 ;
-    strcpy(u1.name , u_name) ;
-    strcpy(u1.password , pswd);
-    u1.next = NULL ;
-    return u1 ;
-}*/
 
 
 User SignUp(User u1)
 {   
-    scanf("%s" , &u1.name) ;
-    scanf("%s" , &u1.password) ;
+    scanf("%s" , u1.name) ;
+    scanf("%s" , u1.password) ;
     if(access(u1.name , F_OK) == 0)
     { 
         printf("username already exists!\n") ;
@@ -36,14 +29,51 @@ User SignUp(User u1)
         fp = fopen(u1.name , "w") ;
         fwrite(&u1 , sizeof(User) , 1 , fp) ;
         fclose(fp) ;
+        authentication = 1 ;                               
         return u1 ;
     }
 }
+
+void Login(User u1)
+{
+    User usr ;
+    scanf("%s" , u1.name) ;
+    scanf("%s" , u1.password) ;
+
+    if(access(u1.name , F_OK))
+    {
+        printf("username not found!") ;
+        fclose(fp) ;
+        exit(0) ;
+    }
+
+    else
+    {
+        FILE * fphelp = fopen(u1.name , "r");
+        fread(&usr , sizeof(usr) , 1 , fphelp) ;
+
+        if(strcmp(usr.password , u1.password) == 0)
+        {
+            authentication = 1 ;
+        }
+        else
+        {   
+            printf("password incorrect!\n") ;
+            exit(0) ;
+        }
+        
+    }
+
+
+}
+
+
 
 int main()
 {      
     User u1 ;
     char option[20] ;
+
     scanf("%s" , option) ;
 
     if(!strcmp(option , "signup"))
